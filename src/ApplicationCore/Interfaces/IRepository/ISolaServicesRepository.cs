d5c77bfa0299e1825134ed941b490c9136e18e66
Jeff FORESTAL <103978554+Jeff-44@users.noreply.github.com>
2025-07-17 07:27:08 -0400

    Add full Services CRUD features

--- src/ApplicationCore/Interfaces/IRepository/ISolaServicesRepository.cs
+++ src/ApplicationCore/Interfaces/IRepository/ISolaServicesRepository.cs
@@ -4,6 +4,7 @@ namespace ApplicationCore.Interfaces.IRepository
 {
     public interface ISolaServicesRepository : IGenericRepository<Service>
     {
+        Task<IEnumerable<Service>> GetAllWithCategoriesAsync();
         Task<IEnumerable<Service>> GetServicesByCategoryAsync(int categoryId);
         Task<IEnumerable<Service>> GetActiveServicesAsync();
     }
